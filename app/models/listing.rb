bd86f028983a35001f6b7730ae25b5eb7e18bb00
be090129 <romain.rongier@gmail.com>
2015-07-31 14:48:55 -0400

    authorize delete photo

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ActiveRecord::Base
   belongs_to :user
 
   has_many :images, :dependent => :destroy
-  accepts_nested_attributes_for :images, :reject_if => lambda { |t| t['photo'].nil? }
+  accepts_nested_attributes_for :images, :reject_if => lambda { |t| t['photo'].nil? }, allow_destroy: true
 
 
 
