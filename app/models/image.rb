8831e7c5a5f9cf1b2c5ff6088695fe272b745d64
be090129 <romain.rongier@gmail.com>
2015-08-03 14:53:35 -0400

    header user

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
   belongs_to :listing
+  belongs_to :user
 
-  has_attached_file :photo, :styles => { :medium => "1024x683>", :thumb => "225x150>" },
-                    :default_url => "images/avatar.png"
+  has_attached_file :photo, :styles => { :medium => "1024x683>", :thumb => "225x150>", :avatar => "100x100#" }, :default_url => "/images/:style/missing.jpg"
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
