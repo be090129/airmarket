9b19ee0e860c6c615941b8eff98f7ae5113eee15
be090129 <romain.rongier@gmail.com>
2015-08-05 17:15:19 -0400

    modification

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,7 +3,6 @@ class Listing < ActiveRecord::Base
   geocoded_by :address, :latitude  => :latitude, :longitude => :longitude
   after_validation :geocode
 
-
   belongs_to :user
   has_many :orders
   accepts_nested_attributes_for :orders, :allow_destroy => true
