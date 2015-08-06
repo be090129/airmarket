edd754dfbc005dac0733b3c8c5eca4fe9b7bac92
be090129 <romain.rongier@gmail.com>
2015-08-06 09:33:21 -0400

    ajout config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.serve_static_assets = true
+  config.serve_static_assets = false
+
+  config.assets.initialize_on_precompile = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
