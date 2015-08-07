c770a29fbdace637542f1d96b7641035c577ecb1
be090129 <romain.rongier@gmail.com>
2015-08-07 09:48:37 -0400

    mise en form mangopay api

--- app/services/mangopay_api.rb
+++ app/services/mangopay_api.rb
@@ -29,22 +29,22 @@ class MangopayApi
     def create_user_id(contact)
       with_mangopay_config() do
         MangoPay::NaturalUser.create({
-                                         :FirstName => contact.first_name,
-                                         :LastName => contact.last_name,
-                                         :Email => contact.email,
-                                         :Address => {
-                                             :AddressLine1 => contact.adressline1,
-                                             :AddressLine2=> "",
-                                             :City=> contact.city,
-                                             :Region=> contact.region,
-                                             :PostalCode=> contact.postalcode,
-                                             :Country=> contact.country,
-                                         },
-                                         :Nationality => contact.nationality,
-                                         :Birthday => contact.birthday.to_time.to_i,
-                                         :CountryOfResidence => contact.country,
-                                         :Tag=> "User"
-                                     })
+          :FirstName => contact.first_name,
+          :LastName => contact.last_name,
+          :Email => contact.email,
+          :Address => {
+            :AddressLine1 => contact.adressline1,
+            :AddressLine2=> "",
+            :City=> contact.city,
+            :Region=> contact.region,
+            :PostalCode=> contact.postalcode,
+            :Country=> contact.country,
+            },
+          :Nationality => contact.nationality,
+          :Birthday => contact.birthday.to_time.to_i,
+          :CountryOfResidence => contact.country,
+          :Tag=> "User"
+        })
       end
     end
 
@@ -73,85 +73,84 @@ class MangopayApi
     def create_user_wallet(contact, owner_id)
       with_mangopay_config() do
         MangoPay::Wallet.create({
-                                    :Owners => [owner_id],
-                                    :Description => contact.first_name+"/"+contact.last_name+ " wallet",
-                                    :Tag => contact.first_name+"/"+contact.last_name+ " wallet",
-                                    :Currency => "EUR"
-                                })
+          :Owners => [owner_id],
+          :Description => contact.first_name+"/"+contact.last_name+ " wallet",
+          :Tag => contact.first_name+"/"+contact.last_name+ " wallet",
+          :Currency => "EUR"
+        })
       end
     end
 
     def update_user_wallet(contact)
       with_mangopay_config() do
         MangoPay::Wallet.update(contact.mangopay_wallet_id,{
-                                                              :Description => "Wallet exclusive user",
-                                                              :Tag => "Updated wallet"
-                                                          })
+          :Description => "Wallet exclusive user",
+          :Tag => "Updated wallet"
+        })
       end
     end
 
     def transfer_between_wallets(debited_wallet_id, credited_wallet_id,debited_user_id, credited_user_id,transfer_amount,fees_amount)
       with_mangopay_config() do
         MangoPay::Transfer.create({
-                                      :AuthorId=>debited_user_id,
-                                      :CreditedUserId=>credited_user_id,
-                                      :DebitedWalletID=>debited_wallet_id,
-                                      :CreditedWalletID=>credited_wallet_id,
-                                      :DebitedFunds=>{:Currency=>"EUR",:Amount=>transfer_amount},
-                                      :Fees=>{:Currency=>"EUR",:Amount=>fees_amount},
-                                      :Tag=>"Location OK, versement des fonds sur la wallet du proprio"
-                                  })
+           :AuthorId=>debited_user_id,
+           :CreditedUserId=>credited_user_id,
+           :DebitedWalletID=>debited_wallet_id,
+           :CreditedWalletID=>credited_wallet_id,
+           :DebitedFunds=>{:Currency=>"EUR",:Amount=>transfer_amount},
+           :Fees=>{:Currency=>"EUR",:Amount=>fees_amount},
+           :Tag=>"Location OK, versement des fonds sur la wallet du proprio"
+        })
       end
     end
 
     def payout(debited_wallet_id,debited_user_id,bank_user_id,transfer_amount,fees_amount,tag)
       with_mangopay_config() do
         MangoPay::PayOut::BankWire.create({
-                                              :AuthorId=>debited_user_id,
-                                              :DebitedWalletId=>debited_wallet_id,
-                                              :DebitedFunds=>{:Currency=>"EUR",:Amount=>transfer_amount},
-                                              :Fees=>{:Currency=>"EUR",:Amount=>fees_amount},
-                                              :BankAccountId=>bank_user_id,
-                                              :Tag=>tag
-                                          })
+           :AuthorId=>debited_user_id,
+           :DebitedWalletId=>debited_wallet_id,
+           :DebitedFunds=>{:Currency=>"EUR",:Amount=>transfer_amount},
+           :Fees=>{:Currency=>"EUR",:Amount=>fees_amount},
+           :BankAccountId=>bank_user_id,
+           :Tag=>tag
+        })
       end
     end
 
     def create_user_bank(contact, owner_id)
       with_mangopay_config() do
         MangoPay::BankAccount.create(owner_id,{
-                                                 :Type => "IBAN",
-                                                 :OwnerName => contact.first_name + " "+ contact.last_name,
-                                                 :OwnerAddress => {
-                                                     :AddressLine1 => contact.adressline1,
-                                                     :AddressLine2=> "",
-                                                     :City=> contact.city,
-                                                     :Region=> contact.region,
-                                                     :PostalCode=> contact.postalcode,
-                                                     :Country=> contact.country,
-                                                 },
-                                                 :UserId => owner_id,
-                                                 :IBAN => contact.iban,
-                                                 :BIC => contact.bic
-                                             }
-        )
+           :Type => "IBAN",
+           :OwnerName => contact.first_name + " "+ contact.last_name,
+           :OwnerAddress => {
+              :AddressLine1 => contact.adressline1,
+              :AddressLine2=> "",
+              :City=> contact.city,
+              :Region=> contact.region,
+              :PostalCode=> contact.postalcode,
+              :Country=> contact.country,
+           },
+           :UserId => owner_id,
+           :IBAN => contact.iban,
+           :BIC => contact.bic
+        })
       end
     end
 
     def create_cb_registration(owner_id)
       with_mangopay_config() do
         MangoPay::CardRegistration.create({
-                                              :UserId=>owner_id,
-                                              :Currency=>"EUR"
-                                          })
+           :UserId=>owner_id,
+           :Currency=>"EUR"
+        })
       end
     end
 
     def update_cb_registration(pre_card_register)
       with_mangopay_config() do
         MangoPay::CardRegistration.update(pre_card_register["Id"],{
-                                                                     :RegistrationData=>pre_card_register["RegistrationData"]
-                                                                 })
+          :RegistrationData=>pre_card_register["RegistrationData"]
+        })
       end
     end
 
@@ -170,14 +169,14 @@ class MangopayApi
     def create_payin(mangopay_account,debit_amount_cents,currency,fees_amount_cents,secureModeReturnURL)
       with_mangopay_config() do
         MangoPay::PayIn::Card::Direct.create({
-                                                 :AuthorId=>mangopay_account.mangopay_user_id,
-                                                 :DebitedFunds=>{:Currency=>currency,:Amount=>debit_amount_cents},
-                                                 :Fees=>{:Currency=>currency,:Amount=>fees_amount_cents},
-                                                 :CreditedWalletId=>mangopay_account.mangopay_wallet_id,
-                                                 :CardId=>mangopay_account.mangopay_card_id,
-                                                 :SecureMode=>"DEFAULT",
-                                                 :SecureModeReturnURL=>secureModeReturnURL
-                                             })
+           :AuthorId=>mangopay_account.mangopay_user_id,
+           :DebitedFunds=>{:Currency=>currency,:Amount=>debit_amount_cents},
+           :Fees=>{:Currency=>currency,:Amount=>fees_amount_cents},
+           :CreditedWalletId=>mangopay_account.mangopay_wallet_id,
+           :CardId=>mangopay_account.mangopay_card_id,
+           :SecureMode=>"DEFAULT",
+           :SecureModeReturnURL=>secureModeReturnURL
+        })
       end
     end
 
