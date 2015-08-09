d19d66887b01a460948521d45c613bf342b5e06e
Romain Rongier <romain.rongier@gmail.com>
2015-08-08 21:17:04 -0400

    redirect destroy admin order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -335,7 +335,7 @@ class OrdersController < ApplicationController
   def destroy
     @order.destroy
     respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
+      format.html { redirect_to admin_orders_url, notice: 'Order was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
