800e89252633f71dc9769e7df7f357b1e30bb414
Romain Rongier <romain.rongier@gmail.com>
2015-08-08 20:51:40 -0400

    ajout users in controller

--- config/initializers/mangopay_initializer.rb
+++ config/initializers/mangopay_initializer.rb
@@ -1,5 +1,5 @@
 MangoPay.configure do |c|
-  c.preproduction =  false
+  c.preproduction = Rails.application.config.mangopay_sandbox
   c.client_id = Rails.application.config.mangopay_clientid
   c.client_passphrase = Rails.application.config.mangopay_passphrase
 end
