fe43e0c27a97b9320a6793a8bb6880166df86685
Romain Rongier <romain.rongier@gmail.com>
2015-08-08 20:48:18 -0400

    sand box

--- config/initializers/mangopay_initializer.rb
+++ config/initializers/mangopay_initializer.rb
@@ -1,5 +1,5 @@
 MangoPay.configure do |c|
-  c.preproduction = Rails.application.config.mangopay_sandbox
+  c.preproduction =  false
   c.client_id = Rails.application.config.mangopay_clientid
   c.client_passphrase = Rails.application.config.mangopay_passphrase
 end
